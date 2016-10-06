b01136a11937b06e6a0f89733be691f0358cb3f1
Keri Lawrence <kerilawrence1216@gmail.com>
2016-10-06 13:09:47 -0400

    add checkin/checkout screen

---
+++ app/src/main/java/me/kglawrence/kerilawrence/book_park/SelectionActivity.java
@@ -0,0 +1,27 @@
+package me.kglawrence.kerilawrence.book_park;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectionActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selection_activity);
+        Button dropOffButton = (Button)findViewById(R.id.selection_check_out);
+        Button pickUpButton = (Button)findViewById(R.id.selection_check_in);
+        pickUpButton.setOnClickListener(this);
+        dropOffButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        boolean checkIn = v.getId() == R.id.selection_check_in;
+        intent.putExtra("checkin",checkIn);
+        startActivity(intent);
+    }
+}
